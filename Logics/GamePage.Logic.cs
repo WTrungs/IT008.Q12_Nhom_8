80c247dcae21073af778fbbfd7719ff3fefb7077
WTrungs <godhander274@gmail.com>
2025-12-20 01:16:29 +0700

    Add GamePage.Logic.cs

---
+++ Logics/GamePage.Logic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Media;
+using TetrisApp.Models;
+
+namespace TetrisApp.Views {
+	public partial class GamePage : Page {
+		
+	}
+}
