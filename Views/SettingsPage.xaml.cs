4f921dcd9013e1d91860d608d02e8832bc0ad771
Huy <24520678@gm.uit.edu.vn>
2025-12-20 16:43:41 +0700

    upadate tab and arrow feature

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Navigation;
+using System.Windows.Input;
 using System.Windows.Media; // Thư viện để dùng MediaPlayer
+using System.Windows.Navigation;
+using System.Windows.Threading;
 using TetrisApp.Models;
 
 namespace TetrisApp.Views
@@ -12,14 +14,96 @@ namespace TetrisApp.Views
         // Khai báo trình phát nhạc cho trang Settings
         private MediaPlayer _clickSound = new MediaPlayer();
 
+        // 1. Dependency Property: Công tắc Hover
+        public bool IsHoverEnabled
+        {
+            get { return (bool)GetValue(IsHoverEnabledProperty); }
+            set { SetValue(IsHoverEnabledProperty, value); }
+        }
+
+        // Lưu ý: Đã sửa typeof(LoginPage) thành typeof(SettingsPage) để tránh lỗi crash
+        public static readonly DependencyProperty IsHoverEnabledProperty =
+            DependencyProperty.Register("IsHoverEnabled", typeof(bool), typeof(SettingsPage), new PropertyMetadata(true));
+
         public SettingsPage()
         {
             InitializeComponent();
+        }
 
-            // Load dữ liệu cũ lên giao diện từ AppSettings
-            MusicToggle.IsChecked = AppSettings.IsMusicEnabled;
-            MusicVolumeSlider.Value = AppSettings.MusicVolume;
-            SfxVolumeSlider.Value = AppSettings.SfxVolume;
+        private void SettingsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => {
+                Keyboard.Focus(this);
+                this.Focus();
+            }));
+        }
+
+        // 3. XỬ LÝ PHÍM TRUNG TÂM
+        private void Page_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // A. LUÔN TẮT HOVER: Hễ đụng vào phím điều hướng là tắt chuột ngay
+            if (e.Key == Key.Down || e.Key == Key.Up ||
+                e.Key == Key.Left || e.Key == Key.Right ||
+                e.Key == Key.Tab)
+            {
+                IsHoverEnabled = false;
+            }
+            // B. LOGIC "ĐÁNH THỨC": Nhảy vào MusicToggle khi chưa chọn gì
+            var currentFocus = Keyboard.FocusedElement;
+            if (currentFocus == this || currentFocus == null)
+            {
+                if (e.Key == Key.Down || e.Key == Key.Up || e.Key == Key.Tab || e.Key == Key.Right)
+                {
+                    if (MusicToggle != null)
+                    {
+                        MusicToggle.Focus();
+                        e.Handled = true;
+                    }
+                    return;
+                }
+            }
+
+            // C. LOGIC DI CHUYỂN UP/DOWN (Chuyển dòng)
+            if (e.Key == Key.Down)
+            {
+                e.Handled = true;
+                MoveFocus(FocusNavigationDirection.Next);
+                return;
+            }
+            else if (e.Key == Key.Up)
+            {
+                e.Handled = true;
+                MoveFocus(FocusNavigationDirection.Previous);
+                return;
+            }
+
+            if (currentFocus == MusicToggle)
+            {
+                if (e.Key == Key.Left)
+                {
+                    MusicToggle.IsChecked = false; 
+                    e.Handled = true;              
+                }
+                else if (e.Key == Key.Right)
+                {
+                    MusicToggle.IsChecked = true;  
+                    e.Handled = true;              
+                }
+            }
+        }
+
+        // Hàm hỗ trợ di chuyển Focus (Helper)
+        private void MoveFocus(FocusNavigationDirection direction)
+        {
+            var focusedElement = Keyboard.FocusedElement;
+            if (focusedElement is UIElement uiElement)
+            {
+                uiElement.MoveFocus(new TraversalRequest(direction));
+            }
+            else if (focusedElement is FrameworkContentElement contentElement)
+            {
+                contentElement.MoveFocus(new TraversalRequest(direction));
+            }
         }
 
         // Hàm hỗ trợ phát tiếng click
