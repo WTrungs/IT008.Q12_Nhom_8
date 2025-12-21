cf8aa60c326035685eb48aec0bb863f56dee1a81
WTrungs <godhander274@gmail.com>
2025-12-21 17:39:51 +0700

    Fix so many bugs

--- Logics/GameEngine.cs
+++ Logics/GameEngine.cs
@@ -1,36 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using Newtonsoft.Json; // Cần cài NuGet: Newtonsoft.Json
+using Newtonsoft.Json;
 
-namespace TetrisApp.Views
-{
-    // Class phụ để lưu cấu trúc dữ liệu game (Bạn có thể để file riêng hoặc để chung ở đây cũng được)
-    public class GameStateData
-    {
-        public int Score { get; set; }
-        public int Level { get; set; }
-        public int Line { get; set; }
-        // Lưu mảng màu (null = trống, mã màu = có gạch)
-        public string[,] BoardColors { get; set; }
-    }
+namespace TetrisApp.Views {
+	public class GameStateData {
+		public int Score { get; set; }
+		public int Level { get; set; }
+		public int Line { get; set; }
+		public string[,] BoardColors { get; set; }
+	}
 
-    public partial class GameEngine
-    {
-        public GameEngine()
-        {
-            Start();
-        }
+	public partial class GameEngine {
+		public GameEngine() {
+			Start();
+		}
 
-        public struct Position
-        {
-            public int row, col;
-            public Position(int row = 0, int col = 0)
-            {
-                this.row = row;
-                this.col = col;
-            }
-        }
+		public struct Position {
+			public int row, col;
+			public Position(int row = 0, int col = 0) {
+				this.row = row;
+				this.col = col;
+			}
+		}
 
 		public class Cell {
 			public bool isFilled = false;
@@ -44,180 +36,139 @@ namespace TetrisApp.Views
 				color = other.color;
 			}
 		}
-        public class Cell
-        {
-            public bool isFilled = false;
-            public string color = "null";
-        }
 
-        const int boardRow = 30;
-        const int boardColumn = 10;
-        Position startPosition = new Position(23, 3);
-        Position currentPosition = new Position(23, 3);
-        int currentScore = 0;
-        int currentLevel = 0;
-        int currentLine = 0;
-        Queue<TetrominoKind> kindQueue = new Queue<TetrominoKind>();
-        int tetrominoState = 0;
-        double dropTick = 0.5;
-        double currentTime = 1.0;
-        public Cell[,] boardGame = new Cell[boardRow, boardColumn];
+		const int boardRow = 30;
+		const int boardColumn = 10;
+		Position startPosition = new Position(20, 3);
+		Position currentPosition = new Position(20, 3);
+		int currentScore = 0;
+		int currentLevel = 0;
+		int currentLine = 0;
+		Queue<TetrominoKind> kindQueue = new Queue<TetrominoKind>();
+		int tetrominoState = 0;
+		double dropTick = 0.5;
+		double currentTime = 1.0;
+		public Cell[,] boardGame = new Cell[boardRow, boardColumn];
 
-        public void Start()
-        {
-            for (int i = 0; i < 2; i++)
-            {
-                kindQueue.Enqueue(GetRandomTetrominoKind());
-            }
-            for (int i = 0; i < boardRow; i++)
-            {
-                for (int j = 0; j < boardColumn; j++)
-                {
-                    boardGame[i, j] = new Cell();
-                }
-            }
-        }
+		public void Start() {
+			for (int i = 0; i < 2; i++) {
+				kindQueue.Enqueue(GetRandomTetrominoKind());
+			}
+			for (int i = 0; i < boardRow; i++) {
+				for (int j = 0; j < boardColumn; j++) {
+					boardGame[i, j] = new Cell();
+				}
+			}
+		}
 
-        // --- [MỚI] HÀM LẤY DỮ LIỆU ĐỂ LƯU ---
-        public string GetSaveDataJson()
-        {
-            var state = new GameStateData
-            {
-                Score = this.currentScore,
-                Level = this.currentLevel,
-                Line = this.currentLine,
-                BoardColors = new string[boardRow, boardColumn]
-            };
+		public string GetSaveDataJson() {
+			var state = new GameStateData {
+				Score = this.currentScore,
+				Level = this.currentLevel,
+				Line = this.currentLine,
+				BoardColors = new string[boardRow, boardColumn]
+			};
+			for (int r = 0; r < boardRow; r++) {
+				for (int c = 0; c < boardColumn; c++) {
+					if (boardGame[r, c] != null && boardGame[r, c].isFilled) {
+						state.BoardColors[r, c] = boardGame[r, c].color;
+					}
+					else {
+						state.BoardColors[r, c] = null;
+					}
+				}
+			}
+			return JsonConvert.SerializeObject(state);
+		}
 
-            for (int r = 0; r < boardRow; r++)
-            {
-                for (int c = 0; c < boardColumn; c++)
-                {
-                    if (boardGame[r, c] != null && boardGame[r, c].isFilled)
-                    {
-                        state.BoardColors[r, c] = boardGame[r, c].color;
-                    }
-                    else
-                    {
-                        state.BoardColors[r, c] = null;
-                    }
-                }
-            }
+		public void LoadFromSaveData(string json) {
+			if (string.IsNullOrEmpty(json)) return;
+			try {
+				var state = JsonConvert.DeserializeObject<GameStateData>(json);
+				if (state == null) return;
+				this.currentScore = state.Score;
+				this.currentLevel = state.Level;
+				this.currentLine = state.Line;
+				for (int r = 0; r < boardRow; r++) {
+					for (int c = 0; c < boardColumn; c++) {
+						if (boardGame[r, c] == null) boardGame[r, c] = new Cell();
 
-            return JsonConvert.SerializeObject(state);
-        }
+						if (!string.IsNullOrEmpty(state.BoardColors[r, c])) {
+							boardGame[r, c].isFilled = true;
+							boardGame[r, c].color = state.BoardColors[r, c];
+						}
+						else {
+							boardGame[r, c].isFilled = false;
+							boardGame[r, c].color = "null";
+						}
+					}
+				}
+			}
+			catch { }
+		}
 
-        // --- [MỚI] HÀM NẠP DỮ LIỆU CŨ ---
-        public void LoadFromSaveData(string json)
-        {
-            if (string.IsNullOrEmpty(json)) return;
-            try
-            {
-                var state = JsonConvert.DeserializeObject<GameStateData>(json);
-                if (state == null) return;
+		public void Update() {
+			RunTickEvent();
+		}
 
-                this.currentScore = state.Score;
-                this.currentLevel = state.Level;
-                this.currentLine = state.Line;
+		bool CheckBlockInBoard(Position pos) {
+			if (pos.row < 0 || pos.col < 0 || pos.col >= 10) return false;
+			return true;
+		}
 
-                // Phục hồi bàn cờ
-                for (int r = 0; r < boardRow; r++)
-                {
-                    for (int c = 0; c < boardColumn; c++)
-                    {
-                        if (boardGame[r, c] == null) boardGame[r, c] = new Cell();
+		public TetrominoKind GetCurrentKind() {
+			return kindQueue.Peek();
+		}
 
-                        if (!string.IsNullOrEmpty(state.BoardColors[r, c]))
-                        {
-                            boardGame[r, c].isFilled = true;
-                            boardGame[r, c].color = state.BoardColors[r, c];
-                        }
-                        else
-                        {
-                            boardGame[r, c].isFilled = false;
-                            boardGame[r, c].color = "null";
-                        }
-                    }
-                }
-            }
-            catch { /* Bỏ qua lỗi file save hỏng */ }
-        }
+		public int GetTetrominoState() {
+			return tetrominoState;
+		}
 
-        public void Update()
-        {
-            RunTickEvent();
-        }
+		public Position GetCurrentPosition() {
+			return currentPosition;
+		}
 
-        // ... (Giữ nguyên các hàm logic cũ bên dưới: CheckBlockInBoard, GetCurrentKind, v.v...) ...
-        // ĐỂ TIẾT KIỆM DÒNG, MÌNH KHÔNG COPY LẠI PHẦN LOGIC CŨ VÌ NÓ KHÔNG ĐỔI.
-        // BẠN GIỮ NGUYÊN PHẦN DƯỚI CỦA FILE GỐC NHÉ.
+		bool CheckValidPosition(Position pos) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					int curRow = pos.row - i;
+					int curCol = pos.col + j;
+					if (tetrominos[GetCurrentKind()][tetrominoState][i, j] == 0) {
+						continue;
+					}
+					Position curPos = new Position(curRow, curCol);
+					if (!CheckBlockInBoard(curPos)) return false;
+					if (boardGame[curRow, curCol].isFilled == true) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 
-        bool CheckBlockInBoard(Position pos)
-        {
-            if (pos.row < 0 || pos.col < 0 || pos.col >= 10) return false;
-            return true;
-        }
+		TetrominoKind GetRandomTetrominoKind() {
+			Random rand = new Random();
+			return (TetrominoKind)rand.Next(0, 7);
+		}
 
-        public TetrominoKind GetCurrentKind()
-        {
-            return kindQueue.Peek();
-        }
-
-        public int GetTetrominoState() { return tetrominoState; }
-        public Position GetCurrentPosition() { return currentPosition; }
-
-        // ... (Copy tiếp phần còn lại của file gốc vào đây) ...
-
-        // Cần đảm bảo hàm thay thế biến tetrominos, tetrominoColor... có quyền truy cập
-        // Nếu file gốc của bạn có khai báo tetrominos ở file Tetromino.cs thì OK.
-
-        // Hết phần sửa, các hàm CheckValidPosition, RunTickEvent... giữ nguyên.
-        // ...
-
-        // VÌ FILE QUÁ DÀI NÊN MÌNH CHỈ NOTE LÀ: 
-        // BẠN CHỈ CẦN THÊM 2 HÀM `GetSaveDataJson` VÀ `LoadFromSaveData` VÀO TRONG CLASS LÀ ĐỦ.
-        // CÁC LOGIC KHÁC KHÔNG CẦN SỬA.
-
-        // --- CHÈN ĐOẠN NÀY ĐỂ TRÁNH LỖI NẾU BẠN COPY PASTE MẤT CODE CŨ ---
-        bool CheckValidPosition(Position pos)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    // Lưu ý: Cần đảm bảo biến tetrominos có thể truy cập được (từ file Tetromino.cs)
-                    int curRow = pos.row - i;
-                    int curCol = pos.col + j;
-                    if (tetrominos[GetCurrentKind()][tetrominoState][i, j] == 0) continue;
-                    Position curPos = new Position(curRow, curCol);
-                    if (!CheckBlockInBoard(curPos)) return false;
-                    if (boardGame[curRow, curCol].isFilled == true) return false;
-                }
-            }
-            return true;
-        }
-
-        TetrominoKind GetRandomTetrominoKind()
-        {
-            Random rand = new Random();
-            return (TetrominoKind)rand.Next(0, 7);
-        }
-
-        void RunTickEvent()
-        {
-            currentTime -= Time.deltaTime;
-            if (currentTime <= 0)
-            {
-                Position newPos = currentPosition;
-                --newPos.row;
-                if (CheckValidPosition(newPos)) currentPosition = newPos;
-                else MakeNewTurn();
-                currentTime = dropTick;
-            }
-        }
+		void RunTickEvent() {
+			currentTime -= Time.deltaTime;
+			if (currentTime <= 0) {
+				Position newPos = currentPosition;
+				--newPos.row;
+				if (CheckValidPosition(newPos)) {
+					currentPosition = newPos;
+				}
+				else {
+					MakeNewTurn();
+				}
+				currentTime = dropTick;
+			}
+		}
 
 		void MakeNewTurn() {
 			FillBlockToBoard();
+			DeleteFilledLine();
 			ResetKindQueue();
 			Position plannedPosition = FindNewPosition();
 			if (DoNotHaveValidBlock(plannedPosition)) {
@@ -227,65 +178,61 @@ namespace TetrisApp.Views
 			currentTime = dropTick;
 		}
 
-        Position FindDeepestPosition()
-        {
-            Position curPos = currentPosition;
-            Position newPos = curPos;
-            newPos.row--;
-            while (CheckValidPosition(newPos))
-            {
-                curPos = newPos;
-                newPos.row--;
-            }
-            return curPos;
-        }
+		Position FindDeepestPosition() {
+			Position curPos = currentPosition;
+			Position newPos = curPos;
+			newPos.row--;
+			while (CheckValidPosition(newPos)) {
+				curPos = newPos;
+				newPos.row--;
+			}
+			return curPos;
+		}
 
-        public void HardDrop()
-        {
-            currentPosition = FindDeepestPosition();
-            MakeNewTurn();
-        }
+		public void HardDrop() {
+			currentPosition = FindDeepestPosition();
+			MakeNewTurn();
+		}
 
-        public void SoftDrop()
-        {
-            Position newPos = currentPosition;
-            --newPos.row;
-            if (CheckValidPosition(newPos))
-            {
-                currentPosition = newPos;
-                currentTime = dropTick;
-            }
-            else MakeNewTurn();
-        }
+		public void SoftDrop() {
+			Position newPos = currentPosition;
+			--newPos.row;
+			if (CheckValidPosition(newPos)) {
+				currentPosition = newPos;
+				currentTime = dropTick;
+			}
+			else {
+				MakeNewTurn();
+			}
+		}
 
-        void ResetKindQueue()
-        {
-            kindQueue.Dequeue();
-            kindQueue.Enqueue(GetRandomTetrominoKind());
-        }
+		void ResetKindQueue() {
+			kindQueue.Dequeue();
+			kindQueue.Enqueue(GetRandomTetrominoKind());
+		}
 
-        bool DoNotHaveValidBlock(Position pos)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    int curRow = currentPosition.row - i;
-                    int curCol = currentPosition.col + j;
-                    if (tetrominos[GetCurrentKind()][tetrominoState][i, j] == 0) continue;
-                    Position curPos = new Position(curRow, curCol);
-                    if (!CheckBlockInBoard(curPos)) return true;
-                }
-            }
-            return false;
-        }
+		bool DoNotHaveValidBlock(Position pos) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					int curRow = currentPosition.row - i;
+					int curCol = currentPosition.col + j;
+					if (tetrominos[GetCurrentKind()][tetrominoState][i, j] == 0) {
+						continue;
+					}
+					Position curPos = new Position(curRow, curCol);
+					if (!CheckBlockInBoard(curPos)) return true;
+				}
+			}
+			return false;
+		}
 
-        Position FindNewPosition()
-        {
-            Position result = startPosition;
-            while (!CheckValidPosition(result)) ++result.row;
-            return result;
-        }
+		Position FindNewPosition() {
+			Position result = startPosition;
+			while (!CheckValidPosition(result)) {
+				++result.row;
+			}
+			return result;
+		}
 
 		bool IsOutOfBoard() {
 			return false;
@@ -309,6 +256,39 @@ namespace TetrisApp.Views
 		}
 
 		void DeleteFilledLine() {
+			List<int> deletedLine = new List<int>();
+			Cell[,] newBoard = new Cell[boardRow, boardColumn];
+			for (int i = 0; i < boardRow; i++) {
+				for (int j = 0; j < boardColumn; j++) {
+					newBoard[i, j] = new Cell();
+				}
+			}
+			int bottom = 0;
+			for (int i = 0; i < 20; i++) {
+				bool isFulled = true;
+				for (int j = 0; j < 10; j++) {
+					if (!boardGame[i, j].isFilled) {
+						isFulled = false;
+						break;
+					}
+				}
+				if (isFulled) {
+					deletedLine.Add(i);
+				}
+				else {
+					for (int j = 0; j < 10; j++) {
+						newBoard[bottom, j] = new Cell(boardGame[i, j]);
+					}
+					++bottom;
+				}
+			}
+			foreach (int i in deletedLine) {
+				MakeEraseLineAnimation(i);
+			}
+			boardGame = newBoard;
+		}
+
+		void MakeEraseLineAnimation(int line) {
 
 		}
 
@@ -324,7 +304,27 @@ namespace TetrisApp.Views
 			int oldState = tetrominoState;
 			tetrominoState = (tetrominoState - 1 + 4) % 4;
 			if (!CheckValidPosition(currentPosition)) {
-				tetrominoState = oldState;
+				if (currentPosition.col < 0) {
+					Position temp = new Position(currentPosition.row, 0);
+					if (CheckValidPosition(temp)) {
+						currentPosition = temp;
+					}
+					else {
+						tetrominoState = oldState;
+					}
+				}
+				else if (currentPosition.col > 6) {
+					Position temp = new Position(currentPosition.row, 6);
+					if (CheckValidPosition(temp)) {
+						currentPosition = temp;
+					}
+					else {
+						tetrominoState = oldState;
+					}
+				}
+				else {
+					tetrominoState = oldState;
+				}
 			}
 		}
 
@@ -343,5 +343,6 @@ namespace TetrisApp.Views
 				currentPosition = newPos;
 			}
 		}
+
 	}
 }
--- Views/GamePage.xaml.cs
+++ Views/GamePage.xaml.cs
@@ -7,180 +7,125 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TetrisApp.Models;
 using TetrisApp.Views;
-using TetrisApp.Services; // [FIX LỖI 1] Thêm dòng này để nhận diện SupabaseService
+using TetrisApp.Services;
 
-namespace TetrisApp.Views
-{
-    public partial class GamePage : Page
-    {
-        private MediaPlayer _clickSound = new MediaPlayer();
-        public GameEngine gameEngine = new GameEngine();
+namespace TetrisApp.Views {
+	public partial class GamePage : Page {
+		private MediaPlayer _clickSound = new MediaPlayer();
+		public GameEngine gameEngine = new GameEngine();
 
-        // Property để MainWindow có thể truy cập Engine lấy dữ liệu save nếu cần
-        public GameEngine Engine => gameEngine;
+		public GameEngine Engine => gameEngine;
 
-        TimeSpan lastRenderTime = TimeSpan.Zero;
+		TimeSpan lastRenderTime = TimeSpan.Zero;
 
-        public GamePage()
-        {
-            InitializeComponent();
-            this.Loaded += GamePage_Loaded;
-            this.Unloaded += GamePage_Unloaded;
-        }
+		public GamePage() {
+			InitializeComponent();
+			this.Loaded += GamePage_Loaded;
+			this.Unloaded += GamePage_Unloaded;
+		}
 
-        // Constructor thứ 2: Dùng khi ấn nút Continue (nhận dữ liệu save)
-        public GamePage(string saveData) : this()
-        {
-            this.Loaded += (s, e) =>
-            {
-                if (!string.IsNullOrEmpty(saveData))
-                {
-                    // Nạp dữ liệu cũ vào Engine
-                    gameEngine.LoadFromSaveData(saveData);
-                    Draw();
-                }
-            };
-        }
+		public GamePage(string saveData) : this() {
+			this.Loaded += (s, e) => {
+				if (!string.IsNullOrEmpty(saveData)) {
+					gameEngine.LoadFromSaveData(saveData);
+					Draw();
+				}
+			};
+		}
 
-        private void GamePage_Loaded(object sender, RoutedEventArgs e)
-        {
-            CompositionTarget.Rendering += OnRender;
-            this.Focusable = true;
-            this.Focus();
-            Keyboard.Focus(this);
-        }
+		private void GamePage_Loaded(object sender, RoutedEventArgs e) {
+			CompositionTarget.Rendering += OnRender;
+			this.Focusable = true;
+			this.Focus();
+			Keyboard.Focus(this);
+		}
 
-        // [FIX LỖI 2] Thêm 'async' vào đây để dùng được 'await' bên trong
-        private async void GamePage_Unloaded(object sender, RoutedEventArgs e)
-        {
-            CompositionTarget.Rendering -= OnRender;
+		private async void GamePage_Unloaded(object sender, RoutedEventArgs e) {
+			CompositionTarget.Rendering -= OnRender;
+			await SaveGameToCloud();
+		}
 
-            // Tự động lưu game khi thoát trang
-            await SaveGameToCloud();
-        }
+		private async System.Threading.Tasks.Task SaveGameToCloud() {
+			try {
+				string json = gameEngine.GetSaveDataJson();
+				await SupabaseService.SaveUserData(json);
+			}
+			catch { }
+		}
 
-        // Hàm phụ trợ lưu game
-        private async System.Threading.Tasks.Task SaveGameToCloud()
-        {
-            try
-            {
-                string json = gameEngine.GetSaveDataJson();
-                // Gọi service lưu lên Cloud
-                await SupabaseService.SaveUserData(json);
-            }
-            catch { /* Bỏ qua lỗi kết nối */ }
-        }
+		private void OnRender(object? sender, EventArgs e) {
+			RenderingEventArgs renderArgs = (RenderingEventArgs)e;
+			if (lastRenderTime == TimeSpan.Zero) {
+				lastRenderTime = renderArgs.RenderingTime;
+			}
+			double deltaTime = (renderArgs.RenderingTime - lastRenderTime).TotalSeconds;
+			lastRenderTime = renderArgs.RenderingTime;
+			Time.UpdateDeltaTime(deltaTime);
+			gameEngine.Update();
+			Draw();
+		}
 
-        private void OnRender(object? sender, EventArgs e)
-        {
-            RenderingEventArgs renderArgs = (RenderingEventArgs)e;
-            if (lastRenderTime == TimeSpan.Zero)
-            {
-                lastRenderTime = renderArgs.RenderingTime;
-            }
-            double deltaTime = (renderArgs.RenderingTime - lastRenderTime).TotalSeconds;
-            lastRenderTime = renderArgs.RenderingTime;
-            Time.UpdateDeltaTime(deltaTime);
-            gameEngine.Update();
-            Draw();
-        }
+		private void Page_KeyDown(object sender, KeyEventArgs e) {
+			if (!e.IsRepeat) {
+				switch (e.Key) {
+					case Key.Up:
+						gameEngine.ChangeStateToLeft();
+						e.Handled = true;
+						break;
+					case Key.Space:
+						gameEngine.HardDrop();
+						e.Handled = true;
+						break;
+				}
+			}
+			switch (e.Key) {
+				case Key.Left:
+					gameEngine.MoveLeft();
+					e.Handled = true;
+					break;
+				case Key.Right:
+					gameEngine.MoveRight();
+					e.Handled = true;
+					break;
+				case Key.Down:
+					gameEngine.SoftDrop();
+					e.Handled = true;
+					break;
+			}
 
-        private void Page_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (!e.IsRepeat)
-            {
-                switch (e.Key)
-                {
-                    case Key.Up:
-                        gameEngine.ChangeStateToLeft();
-                        break;
-                    case Key.Space:
-                        gameEngine.HardDrop();
-                        break;
-                }
-            }
+		}
 
-            switch (e.Key)
-            {
-                case Key.Left:
-                    gameEngine.MoveLeft();
-                    break;
-                case Key.Right:
-                    gameEngine.MoveRight();
-                    break;
-                case Key.Down:
-                    gameEngine.SoftDrop();
-                    break;
-            }
-        }
+		private void Page_KeyUp(object sender, KeyEventArgs e) {
 
-        private void Page_KeyUp(object sender, KeyEventArgs e)
-        {
+		}
 
-        }
+		public void Draw() {
+			GameCanvas.Children.Clear();
+			for (int r = 0; r < 20; r++) {
+				for (int c = 0; c < 10; c++) {
+					if (gameEngine.boardGame[r, c] != null && gameEngine.boardGame[r, c].isFilled) {
+						DrawCell(r, c, gameEngine.boardGame[r, c].color);
+					}
+				}
+			}
+			var currentKind = gameEngine.GetCurrentKind();
+			int[,] shape = gameEngine.tetrominos[currentKind][gameEngine.GetTetrominoState()];
 
-        public void Draw()
-        {
-            GameCanvas.Children.Clear();
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (shape[i, j] != 0) {
+						int r = gameEngine.GetCurrentPosition().row - i;
+						int c = gameEngine.GetCurrentPosition().col + j;
+						if (r >= 0 && r < 20 && c >= 0 && c < 10) {
+							DrawCell(r, c, gameEngine.tetrominoColor[currentKind]);
+						}
+					}
+				}
+			}
+		}
 
-            // Sửa vòng lặp vẽ để khớp với kích thước board (30x10)
-            // Chỉ vẽ 20 dòng dưới cùng để hiển thị
-            for (int r = 0; r < 30; r++)
-            {
-                for (int c = 0; c < 10; c++)
-                {
-                    if (gameEngine.boardGame[r, c] != null && gameEngine.boardGame[r, c].isFilled)
-                    {
-                        DrawCell(r, c, gameEngine.boardGame[r, c].color);
-                    }
-                }
-            }
-
-            var currentKind = gameEngine.GetCurrentKind();
-            // Đảm bảo 'tetrominos' trong GameEngine là public
-            int[,] shape = gameEngine.tetrominos[currentKind][gameEngine.GetTetrominoState()];
-
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (shape[i, j] != 0)
-                    {
-                        int r = gameEngine.GetCurrentPosition().row - i;
-                        int c = gameEngine.GetCurrentPosition().col + j;
-                        // Chỉ vẽ nếu nằm trong vùng hiển thị (ví dụ 0-29 hoặc 0-19 tùy logic render của bạn)
-                        if (r >= 0 && r < 30 && c >= 0 && c < 10)
-                        {
-                            DrawCell(r, c, gameEngine.tetrominoColor[currentKind]);
-                        }
-                    }
-                }
-            }
-        }
-
-        private void DrawCell(int row, int col, string colorCode)
-        {
-            // Xử lý trường hợp màu null hoặc lỗi
-            if (string.IsNullOrEmpty(colorCode) || colorCode == "null") colorCode = "Gray";
-
-            var brush = new BrushConverter().ConvertFrom(colorCode) as SolidColorBrush;
-            Rectangle rect = new Rectangle
-            {
-                Width = 25,
-                Height = 25,
-                Fill = brush ?? Brushes.Gray,
-                RadiusX = 4,
-                RadiusY = 4
-            };
-
-            // Logic vẽ cũ của bạn (có thể cần điều chỉnh nếu boardRow=30)
-            Canvas.SetTop(rect, (19 - row) * 26);
-            Canvas.SetLeft(rect, col * 26);
-            GameCanvas.Children.Add(rect);
-        }
 		private void DrawCell(int row, int col, string colorCode) {
 			var brush = new BrushConverter().ConvertFrom(colorCode) as SolidColorBrush;
-
 			Rectangle rect = new Rectangle {
 				Width = 25,
 				Height = 25,
@@ -193,23 +138,20 @@ namespace TetrisApp.Views
 			GameCanvas.Children.Add(rect);
 		}
 
-        private void PlayClickSound()
-        {
-            try
-            {
-                string soundPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets/click.mp3");
-                _clickSound.Open(new Uri(soundPath));
-                _clickSound.Volume = AppSettings.SfxVolume;
-                _clickSound.Stop();
-                _clickSound.Play();
-            }
-            catch { }
-        }
+		private void PlayClickSound() {
+			try {
+				string soundPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets/click.mp3");
+				_clickSound.Open(new Uri(soundPath));
+				_clickSound.Volume = AppSettings.SfxVolume;
+				_clickSound.Stop();
+				_clickSound.Play();
+			}
+			catch { }
+		}
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            PlayClickSound();
-            NavigationService?.Navigate(new Uri("Views/MenuPage.xaml", UriKind.Relative));
-        }
-    }
+		private void BackButton_Click(object sender, RoutedEventArgs e) {
+			PlayClickSound();
+			NavigationService?.Navigate(new Uri("Views/MenuPage.xaml", UriKind.Relative));
+		}
+	}
 }
